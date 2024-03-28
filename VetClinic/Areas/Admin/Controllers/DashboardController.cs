28cf776d28635f2028cb3b6469d9c87126777a44
IrinaB <balkarovairina@gmail.com>
2024-03-28 10:07:05 +0100

    Add Admin Layout

---
+++ VetClinic/Areas/Admin/Controllers/DashboardController.cs
@@ -0,0 +1,33 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using VetClinic.Models;
+
+namespace VetClinic.Controllers
+{
+    [Area("Admin")]
+    public class DashboardController : Controller
+    {
+        private readonly ILogger<DashboardController> _logger;
+
+        public DashboardController(ILogger<DashboardController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
